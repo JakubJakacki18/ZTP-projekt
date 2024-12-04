c6fd21c8bb6a9ce5abdab582e949508402f2ab1b
Jakub <jakub.jakacki18@gmail.com>
2024-12-04 21:26:15 +0100

    - ingredient and categoryEnum

---
+++ Model/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTP_projekt.Data.Enum;
+
+namespace ZTP_projekt.Model
+{
+	internal class Ingredient
+	{
+		int id;
+		string name;
+		CategoryEnum categoryEnym;
+		int quantity;
+	}
+}
