7bd08e9c9023a89c16aac73bb9985c1a7e73a714
Klaudia Klim <klaudia.n.klim@gmail.com>
2025-01-19 14:32:13 +0100

    Builder in recipes

--- Model/Recipe.cs
+++ Model/Recipe.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using ZTP_projekt.Interface;
 using ICloneable = ZTP_projekt.Interface.ICloneable;
 
-
 namespace ZTP_projekt.Model
 {
     internal class Recipe : IMealComposite, ICloneable
@@ -20,23 +19,32 @@ namespace ZTP_projekt.Model
             Ingredients = new List<Ingredient>();
             Calories = calories;
         }
-       
+
         public void AddIngredient(Ingredient ingredient, int calories)
         {
             Ingredients.Add(ingredient);
         }
+
         public void ChangeIngredient(int numberOfIngredient, Ingredient ingredient, int calories)
-		{
-			throw new NotImplementedException();
-		}
-        public void RemoveIngredient(int numberOfIngredient) 
-        {
-            throw new NotImplementedException();
-		}
-		public void Display()
         {
             throw new NotImplementedException();
         }
+
+        public void RemoveIngredient(int numberOfIngredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCalories(int calories)
+        {
+            Calories = calories;
+        }
+
         public object Clone()
         {
             Recipe clonedRecipe = (Recipe)this.MemberwiseClone();
@@ -46,6 +54,7 @@ namespace ZTP_projekt.Model
             {
                 clonedRecipe.Ingredients.Add(new Ingredient(ingredient.Id, ingredient.Name, ingredient.Quantity));
             }
+            clonedRecipe.Calories = this.Calories;
 
             return clonedRecipe;
         }
